8c65b27cf3733527755da669cb43201ea07b589a
sureshmanikandanK <118232549+sureshmanikandanK@users.noreply.github.com>
2024-06-22 13:31:15 +0530

    Delete organicshop directory

--- organicshop/src/Routing/routing.js
+++
@@ -1,8 +0,0 @@
-import { createBrowserRouter } from "react-router-dom"
-import LoginPageComp from "../Access/LoginPageComp";
-
-const router = createBrowserRouter([
-
-    {path:"Login",element:<LoginPageComp/>},
-])
-export default router;
