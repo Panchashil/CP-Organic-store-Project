76ee295e03ace3f80761de37d3c1d9589d34bd33
Sureshmanikandan K <sureshmanikandan.k>
2024-06-22 13:32:59 +0530

    Suresh-FisrtUpload

---
+++ src/routing/routing.js
@@ -0,0 +1,58 @@
+import { createBrowserRouter } from "react-router-dom"
+import MySliderComp from "../components/MySliderComp"
+import MyImagesComp from "../components/MyImages";
+import ParentComp from "../components/ParentComp";
+import PageNotFound from "../components/PageNotFound";
+import ReactHooksComp from "../Hooks/ReactHooksComp";
+import UseStateHookComp from "../Hooks/UseStateHookComp";
+import UseEffectHookComp from "../Hooks/UseEffectHookComp";
+import MaindashboardComp from "../Layout/MaindashboardComp";
+import NavComp from "../Layout/NavComp";
+import VirtualDomComp from "../components/VirtualDomComp";
+import FormValComp from "../components/FormValComp";
+import ProductDashboardComp from "../CRUD/ProductDashboardComp";
+import ProductAddComp from "../CRUD/ProductAddComp";
+import ProductUpdateComp from "../CRUD/ProductUpdateComp";
+import LoginPage from "../Layout/LoginPage";
+import SignOut from "../Layout/Signout";
+import ProtectedRoutingComp from "./ProtectedRoutingComp";
+import RegisterPageComp from "../Layout/RegisterPageComp";
+
+
+const router = createBrowserRouter([
+    {path:"",element:<LoginPage/>},
+    {path:"Login",element:<LoginPage/>},
+    {path:"Register",element:<RegisterPageComp/>},
+    {path:"SignOut",element:<SignOut/>},
+    {path:"SignOut/:id",element:<LoginPage/>},
+    {path:"Maindashboard",
+        // element:<MaindashboardComp/>
+        element:<ProtectedRoutingComp Component={MaindashboardComp}/>,children:[
+        {path:"nav",element:<NavComp/>},
+        // {path:"Copyright",element:</>},
+        {path:"",element:<MySliderComp/>},
+        {path:"myslider",element:<MySliderComp/>},
+        {path:"ProductDashboard",element:<ProductDashboardComp/>},
+        {path:"ProductUpdate/:id",element:<ProductUpdateComp/>},
+        {path:"ProductAdd",element:<ProductAddComp/>},
+        {path:"SignOut",element:<SignOut/>},
+        {path:"myimages",element:<MyImagesComp/>},
+        {path:"parent/:id",element:<ParentComp/>},
+        {path:"parent",element:<ParentComp/>},
+        {path:"FormValComp",element:<FormValComp/>},
+        {path:"VirtualDom",element:<VirtualDomComp/>},
+        {path:"UseState",element:<UseStateHookComp/>},
+        {path:"ReactHooks",element:<ReactHooksComp />,children:[
+            {path:"UseState",element:<UseStateHookComp/>},
+            {path:"UseEffect",element:<UseEffectHookComp/>},
+        ]},
+    ]},
+    
+ 
+    {path:"*",element:<PageNotFound/>},
+
+])
+
+
+export default router;
+
---
+++ src/shared/constant/ConstantData.js
@@ -0,0 +1,19 @@
+import Tiger from '../images/tiger-jpg.jpg'
+import Building from '../images/jpg_44-2.jpg'
+import Panda from '../images/Panda.jpg'
+import nature from '../images/Nature.jpg'
+import mountain from '../images/Mountain.jpg'
+import nature2 from '../images/Nature2.jpg'
+import redflower from '../images/Red flower.jpg'
+import color from '../images/color.jpg'
+import Organic from '../images/photo-1625246333195-78d9c38ad449.avif'
+import Organic2 from '../images/Organic2.jpg'
+import Organic3 from '../images/Organic3.jpg'
+import Organic4 from '../images/tractor-5091577_1280.jpg'
+import sampleVideo from '../images/Video.mp4'
+
+
+const staticData = {
+    Tiger,Building,Panda,nature,mountain,nature2,redflower,sampleVideo,color,Organic,Organic2,Organic3,Organic4
+}
+export default staticData;
